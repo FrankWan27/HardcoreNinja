df021a03d2122a5c20a72b2b76d6cb706d2e6d6e
FrankWan27 <frankwan27@gmail.com>
2019-03-28 01:43:31 -0700

    wasd movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        //Calculate movement as a 3D vector
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+       
+
+        Vector3 moveHorizontal = transform.right * xMove; //(1, 0, 0)
+        Vector3 moveVertical = transform.forward * zMove; //(0, 0, 1)
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        motor.Move(velocity);
+
+        //Calculate rotation
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+    }
+}
